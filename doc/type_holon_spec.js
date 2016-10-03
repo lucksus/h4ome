5f366e724ebd6ea255a42c6b18a6d41256a6b6a4
Nicolas Luck <nicolas@lucksus.eu>
2016-10-04 00:16:44 +0200

    Spec examples for Holon, ActiveHolon and TypeHolon.

---
+++ doc/type_holon_spec.js
@@ -0,0 +1,26 @@
+{
+  // ... every holon attribute ... (see holon_spec.js and all other *_holon_spec.js)
+
+  _type_holon_name: 'Spreadsheet Cell',
+
+  _type_holon_attributes: [
+    { 'value': 'string' }
+  ]
+
+  _type_holon_edges: [
+    // TBD
+  ],
+
+  _type_holon_edge_types: [
+    // TBD
+  ]
+
+  _type_holon_subjects: [
+    {
+      type: 'Spreadsheet Cell',
+      edge_type: undefined, //any edge type
+      quantity: 'unlimited',
+      required: false
+    }
+  ]
+}
