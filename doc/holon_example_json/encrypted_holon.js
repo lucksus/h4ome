b728fefae36dc6d503c90daa63c56f06d770e26b
Nicolas Luck <nicolas@lucksus.eu>
2016-10-04 22:29:42 +0200

    Added encrypted and signed holon

---
+++ doc/holon_example_json/encrypted_holon.js
@@ -0,0 +1,35 @@
+{
+  // The whole holon is stored encrypted potentially multiple times for different keys
+  _encrypted_holon: {
+    {
+      // Every encrypted part can use a different block cipher
+      cipher: 'AES-256',
+
+      // The public key of the ID that can decrypt this cipher text
+      pubkey: 'ABAAACAQDPsRJBJerQipobu6paEPRUdtyxy15+OO+z7QDKRUlL8K',
+
+      // The encrypted bytes
+      cipher_text: |
+        ahskxDODhdCpwNm7ansF3qGavTcvBg47U5YqIYY+9X0kZpazUSwNW6CN+0+
+        NZUPOMUkV5DZUTclfIQev08/GExVLPo7SiBomPiXJ3V4MXY6mfdDlDx9lpCc
+        FxYPB6rG9cTY3Nt32mGgcEPv0pQa0MxpNr+ImRqrSoOEDDelvx3JIjjVOqNA
+        a4aM/NnbMoF1Ro68HuOetUw5fGa1ghlMJv4uPi9wchrAPlLL7lW5DCaHYjqO
+        +UhszlvKEN2AD4NSlxgbErRx4wAWDPNKyqc2me7LVynQ4QHTpnvnlQooG8kZ
+        bHfzG+g5IsMLwUi9zrz6RAcKSBccJNeVPKoalh/mWxacBakcMEQZa6WGpMwv
+        6WCCreNURbs7trVZc7RKrF6yLxpIX82gA+3Oe5QG7pshU/6hhcf+v/yRrNJs
+        1QkipEUy+AJRt5eNlTyURYmPlSpeQNYZ70Q/ZMs1t8ss4o0RU3/ZkPwg5zgO
+        680ZI19HWxdka0uIpCbYkUM9INDZXL1UmeeIccKoPw1SinDWTnCfWb/cShQj
+        aUrlXdyPjhVwq41pAj4uS75f/GzQhWZlerYiTrQQkgHO04I2ujc2zLv3qeo
+    },
+    {
+      cipher: 'AES-256',
+      pubkey: 'iFgopSJaL1KlR7zRScCU9HxnIEnH2f41ahskxDODhdCpwNm7ansF',
+      cipher_text: |
+        zUSwNW6CN+0+NZUPOMUkV5DZUTclfIQev08/GExVLPo7SiBomPiXJ3V4MXY6
+        mfdDlDx9lpCcFxYPB6rG9cTY3Nt32mGgcEPv0pQa0MxpNr+ImRqrSoOEDDel
+        vx3JIjjVOqNAa4aM/NnbMoF1Ro68HuOetUw5fGa1ghlMJv4uPi9wchrAPlLL
+        7lW5DCaHYjqO+UhszlvKEN2AD4NSlxgbErRx4wAWDPNKyqc2me7LVynQ4QHT
+        pnvnlQooG8kZbHfzG+g5IsMLwUi9zrz6RAcKSBcc
+    }
+  }
+}
---
+++ doc/holon_example_json/signed_holon.js
@@ -0,0 +1,17 @@
+{
+  _signed_holon_content: {
+    // the whole holon goes here ...
+  },
+
+  _signed_holon_signature: {
+    pubkey: 'ABAAACAQDPsRJBJerQipobu6paEPRUdtyxy15+OO+z7QDKRUlL8K',
+    signature: |
+      iQEcBAEBCAAGBQJX8jrlAAoJEBtgpGLRdMrfRr4H/21BO++l3AAtwQWNvKq9L5O3
+      sR9lXgr6V/FpA+VQjpFl/FAlGhjgLbJkNcwlTzcrwWX5Z9FoA3fX9JYEoPSK4wZt
+      pg5nPXxkrCfJtPXZP6Fy/HQGtqSp7eyTYwZereVk595MTu/P4IPKMAnIwZgN6DXW
+      HrdViTElj6xt2aB3P5ABE7mQLY95ifaltbkX+6KW5OsuoSyGCMy+HzKXuiZUUcMf
+      x8TenGUFrDyisLxe0tXJdk9n1jIU3ycrDNIC5oQ/Y+Fu6NMiwrvap64hU2IC0ftm
+      vzjpgqIDjNsh3+RAJzpi3eq8KGWxcODXdibROfI8BbdbNSD4n0kXFoJDtXUPsV0=
+      =f/nh
+  }
+}
