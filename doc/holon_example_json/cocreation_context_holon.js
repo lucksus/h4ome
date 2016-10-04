95d8c09dae01faf367d75d3f650609d21bb4fab1
Nicolas Luck <nicolas@lucksus.eu>
2016-10-04 22:48:01 +0200

    Cocretion context holon

---
+++ doc/holon_example_json/cocreation_context_holon.js
@@ -0,0 +1,22 @@
+{
+  // A cocreation context is a shared, forked or virtual namespace
+  // where all co-creators can write to.
+  // It could represent a telepresence instance and thus be the shared
+  // consensus information between all participants of a telepresence session.
+  // It could also act as a pull-request when thrown into the inbox of
+  // its root holon.
+
+  // The root holon is the main branch to which this namespace represents
+  // a forked version. So this is the holon that was altered by this session
+  // and against which these changes should be applied.
+  _root_holon: 'Qmasdnlwsakdnt32',
+
+  _holon_nodes: {
+    "intention1": "Qmasdnlwsakdnt32",
+    "vision1": "Qmasdinwlijsa"
+  },
+
+  _holon_edges: [
+    // ...
+  ]
+}
