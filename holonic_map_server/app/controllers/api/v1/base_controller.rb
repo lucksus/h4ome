c77ddda454cc89ad575c854e94f7747e6d0781be
Nicolas Luck <nicolas@lucksus.eu>
2016-10-07 03:00:18 +0200

    Errors within json response

--- holonic_map_server/app/controllers/api/v1/base_controller.rb
+++ holonic_map_server/app/controllers/api/v1/base_controller.rb
@@ -2,6 +2,20 @@ class API::V1::BaseController < ApplicationController
   include API::Response::ResponseHelper
   include API::Request::Authentication
   skip_before_action :verify_authenticity_token
+  rescue_from StandardError, with: :error
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
+
+  protected
+
+  def error(exception)
+    raise exception unless env["ORIGINAL_FULLPATH"] =~ /^\/api/
+
+    add_error('Internal server error', exception)
+    set_status 500
+    render template: 'api/v1/empty.json.jbuilder', format: :json
+  end
+
 
   def render_error(message, status=500)
     add_error(message)
