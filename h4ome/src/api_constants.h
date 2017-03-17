ec3e1dfc4edc7a4052c95584adb3bef4046ed7ba
Nicolas Luck <nicolas@lucksus.eu>
2017-03-18 00:43:27 +0100

    Switch to backend in Dortmund

--- h4ome/src/api_constants.h
+++ h4ome/src/api_constants.h
@@ -1,7 +1,7 @@
 #ifndef API_CONSTANTS_H
 #define API_CONSTANTS_H
 
-#define API_BASE_URL "https://h4ms.h4ome.io/api/v1/"
+#define API_BASE_URL "https://h4ms.rosental10.de/api/v1/"
 //#define API_BASE_URL "http://localhost:3000/api/v1/"
 
 #endif // API_CONSTANTS_H
